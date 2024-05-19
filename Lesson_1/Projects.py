e9d896bf0ce09bc5a61ae267c7272b917c2e0977
Asror Abdurazoqov <asrorbekabdurazzoqov99@gmail.com>
2024-05-19 22:30:40 +0500

    ojipipikjpo

---
+++ Lesson_1/Projects.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 16 07:43:46 2024
+
+@author: Asilbek
+"""
+# Asror comment 1
+#cars=["toyota","mazda","hyundai","gm","kia"]
+#for car in cars:
+ #   if car=="gm":
+  #      print(car.upper())
+   # else:
+   #     print(car.title())
+#cars=["toyota","mazda","hyundai","gm","kia"]
+#for car in cars:
+ #     if car!="gm":
+ #         print(car.upper())
+ #     else:
+  #        print(car.title())
+#ism=input("Ismingiz nima?\n>>>")
+#if ism.lower()=="admin":    
+#    print(f"Xush kelibsiz,{ism.title()}")
+#else:
+ #   print(f"Xush kelibsiz ,{ism.title()}")
+#x=float(input("Birinchi sonni kiriting:"))
+#y=float(input("Ikkinchi sonni kiriting:"))
+#if x==y:
+#    print(f"Sonlar teng:{x}={y}")
+#x=float(input("Birinchi sonni kiritig:"))
+#y=float(input("Ikkinchi sonni kriting:"))
+if x==y:
+    print(f"Sonlar teng:{x}={y}")
+son=float(input("Istalgan son kiriting:"))
+if son<0:
+    print("son manfiy")
+else:
+    print("Son musbat")
+son=float(input("Istalgan son kiriting:"))
+if son>0:
+    print(son**(1/2))
+else:
+    print("Musbat son kiriting")
+     
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
---
+++ Lesson_1/books.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 19 22:29:24 2024
+
+@author: Asilbek
+"""
+
