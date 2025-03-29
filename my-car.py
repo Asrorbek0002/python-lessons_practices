153dd04bf01b58b8ebb031067ded4038375c415d
Asror Abdurazoqov <asrorbekabdurazzoqov99@gmail.com>
2025-03-29 18:05:45 +0500

    Create my-car.py

---
+++ my-car.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  7 19:33:04 2024
+
+@author: Asilbek
+"""
+
+# Tartiblash bizga malumki haqanday narsani tartiblash mumkun shu sababli shu yerdada tartiblash bo'limida biz tanishamiz har doimgida
+#soncars=["bmw","mercedes bens","volvo","general motors","tesla","audi"]
+narhlar=[12000,22500,23456,9800,5600,9934,32874]
+qimmat=max(narhlar)
+arzon=min(narhlar)
+jami=sum(narhlar)
+print("Eng qimmat narh",qimmat,"Eeng arzon narh",arzon,"jami summa",jami)
+cars=["bmw","ercedes benz","general motors","volve","tesla","audi"]
+toys=("bus","car","bear","dino","snake","lizard")
