2b302e43e494644362f72a587d8a8cb763a9240e
Asror Abdurazoqov <asrorbekabdurazzoqov99@gmail.com>
2025-03-29 17:58:49 +0500

    Create computer programmer.py

---
+++ computer programmer.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  1 15:05:35 2024
+
+@author: Asilbek
+"""
+
+print("Hellow world!")
+print(7+9)
+print(5516594-54634843488)
+print("hello asror" )
+print(7+8)
+print(23*50+54)
+print("asror in the world")
+print("asror is not only very clever but olsa pretty strong in my opinion")
