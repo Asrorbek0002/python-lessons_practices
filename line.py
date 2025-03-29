6b25d40154d3f8b854c48d69ab004944203f8d3b
Asror Abdurazoqov <asrorbekabdurazzoqov99@gmail.com>
2025-03-29 18:04:04 +0500

    Create line.py

---
+++ line.py
@@ -0,0 +1,125 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 17 16:17:37 2024
+
+@author: Asilbek
+"""
+
+#hozir hatolar bilan ihlashni o'rganamiz shu yerda bizga malumki hatolar 3 turga bo'linadi 
+#birinchisi dastur yozishda sytacs xatoliklar buni yaxshi sifa tidastur bajarilishdan oldin uni  sizga ko'rsatib turadi 
+#syntaxError bilan tanishamiz
+#print("hello world!")
+#print("Hello world!")
+# biz indentation bilan tanishamiz bu yerda bu qator tashlash deyiladi bizga malumki
+#IndentationError  bunda joy tashlash kerak emas o'zi joy tashlash joyi keladi biz bilamiz 
+    
+#print("Hello world!") 
+# endi bu yerda joy tashlash kerak bo'ladi  
+#print("O'ngacha sanaymiz")   
+#for n in range(10):
+  #  print(n) #har doim to'rta tashlash kerak yoki tab tugmasinni bosish kerak bizga malumki
+ #   print(n+1)      
+# RuntimeError dastur bajarilishdan oldin bajariladi maydi dastur bajariliganda bu to'xtab qoladi keyin u to'xtab qoladi bizga malumki
+#typeError
+#son=input("Istalgan sonni kiriting:")
+#son=int(son)
+#print(f"{son} ning kvadrati {son**2} ga teng")        
+#print("Hello world!")     
+#mevalar=["olma","uzum","nok","anor","anjir"]
+#for meva in mevalar:
+ #  print(meva)         
+# Value Error bizga malumki 
+#son=float(input("Istalgan sonni kiriting:>>>"))    
+#if son>=0:
+ #   print("Musbat son")
+#else:
+  #  print("Manfiy son")        
+#index Error bizga malumki deb ataladi
+
+#mevalar=["olma","anor","uzum"]
+#print(mevalar[2])    
+#ZeroDivisinError bizga malumki har qanday so'zlarda 
+#x,y=50,50#nolga bo'losh hatoligi 
+#z=250/(x-y)    
+#mantiqiy hatolar
+#radius=5
+#pi=3.14
+#aylana_yuzi=pi*radius**2
+#print(aylana_yuzi)    
+#son=float(input("Istalgan sonni kiriting:>>>"))
+#ildiz=son**0.5
+#print(f"{son} ning ildizi {ildiz} ga teng")    
+#mevalar=["olma","anor","uzum","nok"]
+#for meva in mevalar:
+#    print(meva)
+#print("Dastur tugadi")
+#uyga vaifani bajarish kerak
+#son=float(input("Juft son kiriting:"))
+#if son%2==0:
+ #   print("Bu son juft emas.")
+#else:
+#    print("Rahmat")    
+#yosh=float(input("Yoshingiz nechida?"))
+#if yosh<=4 or yosh>=60:
+#    narh=0
+#elif yosh<18:
+#    narh=10000
+#else:
+ #   narh=20000
+  #  print(f"CHipta {narh} so'm")     
+#=float(input("Birinchi sonni kiriting"))
+#y=float(input("Ikkinchi sonni kiriting")) 
+#if x==y:
+#    print(f"{x}={y}")
+#elif x<y:
+ #   print(f" {x}<{y}")
+#else:
+   # print(f" {x}>{y}")          
+#mahsulotlar=["un","yog'","sovun","tuxum","piyoz","kartoshka","olma","banan","uzum","qovun"] 
+#sevat=[]
+#for n in range(5):
+  # sevat.append(input(f"Savatga {n+1}-mahsulotni qo'shish "))
+#if sevat:
+ #   for mahsulot in sevat:
+       # if mahsulot in mahsulotlar:
+      #  else:
+            
+            
+          # print("Savatingiz bo'sh")
+    
+#mahsulotlar=["un","yog'","sovun","tuxum","piyoz","kartoshka","olma","banan","uzum","qovun"] 
+#savat=[]
+#for n in range(5):
+#    savat.append(input(f"Savatga {n+1}-mahsulotni qo'shing:"))   
+#bor_mahsulotlar=[]
+#mavjud_emas=[] 
+#for mahsulot in mahsulotlar:
+ #   if mahsulot in mahsulotlar:
+  #      bor_mahsulotlar.append(mahsulotlar)
+ #   else:
+   #     mavjud_emas.append(mahsulot)
+#if mavjud_emas:
+ # print("Do'konimizda quyidagi mahsulotlar yo'q:") 
+#for mahsulot in mavjud_emas:
+#    print(mahsulot)
+#else:
+   # print("Siz so'ragan barcha mahsulotlar do'konimizda bor") 
+users=["alisher1983","aziza","yasina","umar" ]     
+login=input("YANGI LOgin tanlang:")
+if login in users:
+    print("Login band,yangi login tanlang!")
+else:
+    print("Xush kelibsiz")    
+ 
+ 
+    
+ 
+ 
+
+
+
+
+
+
+
+   
