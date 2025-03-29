c804ad7fd180a9dddc5c674e25134577c1da1481
Asror Abdurazoqov <asrorbekabdurazzoqov99@gmail.com>
2025-03-29 18:07:48 +0500

    Create my-life.py

---
+++ my-life.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May  3 14:56:49 2024
+
+@author: Asilbek
+"""
+
+ism=('Asrorbek Aburazoqov')
+yosh=25
+print(ism)
+print(yosh)
+a=25
+b=6
+c=(a+b)**5
+print(c)
+ism='Asilbek'
+print(ism)
+davlat_qita="uzbekiston"
+print(davlat_qita)
