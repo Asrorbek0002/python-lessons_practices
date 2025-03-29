9d0ca25962770ec53f6de82ab6917e1bba6a4bbb
Asror Abdurazoqov <asrorbekabdurazzoqov99@gmail.com>
2025-03-29 18:09:56 +0500

    Create uk_greatBritin.py

---
+++ uk_greatBritin.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May  3 15:22:25 2024
+
+@author: Asilbek
+"""
+
+ism='I am going to visit foreign countries in orden to study and I am about to take a lot of knowlege as well as increasing wide horizon in my opinion'
+print(ism)
+ism='Asilbek who is a student at medical university which is situated in my country in the east of acountry'
+print(ism)
+yosh=45
+print(yosh)
