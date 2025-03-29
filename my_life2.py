e23692177a21b5dd7293f2652dc9dd3504b24335
Asror Abdurazoqov <asrorbekabdurazzoqov99@gmail.com>
2025-03-29 18:08:54 +0500

    Create my_life2.py

---
+++ my_life2.py
@@ -0,0 +1,138 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 17 08:27:03 2024
+
+@author: Asilbek
+"""
+
+#juft_son=int(input("Juft sonni kiriting:"))
+#if juft_son%2:
+#    print("Bu juft son emas")
+#else:
+ #   print("Rahmat!") 
+#yosh=int(input("Yoshingiz nechida?>>>")) 
+#if yosh<=4 or yosh>60:
+ #  narh=0
+#elif yosh<=18:
+#   narh=10000
+#else:
+#   narh=20000    
+      
+#print(f"Jami  {narh} so'm")
+#y=float(input("Ikkinchi sonni kiriting:"))
+#if x==y:
+ #   print(f"{x}={y}")
+#elif x>y:
+ #   print(f"{x}>{y}")
+#else:
+ #   print(f"{x}<{y}")    
+#x=float(input("Birinchi sonni kiriting:"))
+#y=float(input("Ikkinchi sonni kiriting:"))
+#if x>y:
+ #   print(f"{x}>{y}")
+#elif x==y:
+  #  print(f"{x}={y}")
+#else:
+ #  print(f"{x}<{y}")          
+mahsulotlar=["olma","nok","mandarin","behi","olch","kartoshka","piyoz","sabzi","chesnok","mosh"]
+savat=[]
+#for n in range(5):
+ #   savat.append(input(f"Savstga {n+1}- mahsulotni qo'shing:"))
+    
+
+#for mahsulot in savat:
+ #   if mahsulot in mahsulotlar:
+ #       print(f"Do'konda {mahsulot} bor")
+  #  else:
+   #     print(f"Dokonda {mahsulot} yo'q")
+        
+#mahsulotlar=["olma","nok","mandarin","behi","olch","kartoshka","piyoz","sabzi","chesnok","mosh"]  
+#savat=[]
+#for n in range(5):
+ #  savat.append(input(f"Savatga {n+1}-mahsulotni qo'shing:"))    
+  # bor_mahsulotlar=[]
+   #mavjud_emas=[]
+  # for mahsulot in savat:
+   #    if mahsulot in mahsulotlar:
+    #       bor_mahsulotlar.append(mahsulot)
+     #  else:
+      #     mavjud_emas.append(mahsulot)
+       
+        
+       
+#if mavjud_emas:
+ #  print("Do'konimizda quyidagi mahsulotlar yoq")
+  # for mahsulot in mavjud_emas:
+  #     print(mahsulotlar)
+#else:
+ # print("Siz so'ragan barcha mahsulotlar do'konimizda bor")       
+#users=["asror","asilbek","abror","ayubxon","yunus"]
+#login=input("Yangi login tanlang:")
+#if login.lower() in users:
+ #   print("Login band,yangi login tanlang!")
+#else:
+ #   print(f"Xush kelibsiz, {login.title()}!")    
+sonlar=int(input("Istalgan butun sonni kiriting:"))
+for n in range(2,11):
+    if not (sonlar%n):
+        print(f"{sonlar} sonni {n} ga qoldiqsiz bo'linadi")
+        
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+                
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#savat.append("nok")
+#savat.append("behi")
+#savat.append("olch")
+#savat.append("kartoshka")
+#savat.append("mosh")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
