63774191690bf4abbb219bab81d708d5f155b0aa
Asror Abdurazoqov <asrorbekabdurazzoqov99@gmail.com>
2025-03-29 18:03:04 +0500

    Create keys.py

---
+++ keys.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May  3 15:45:54 2024
+
+@author: Asilbek
+"""
+
+look="Hello World!"
+print(look)
+xabar='my phone is very expensive since whenever I look at it so I feel the sense of satisfaction'
+print(xabar)
+xabar='I live in big city'
+print(xabar)
+radius=5
+pi=3.14159
+aylanani_yuzi='pi*(radius)**2'
+# this is amazing 
+print("Radius",radius,"ga teng bo'gan aylanani yuzi",pi*(radius)**2)
