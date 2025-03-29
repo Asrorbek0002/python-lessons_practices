85cd586295580bca6629c88c4b5fa366e8e7d020
Asror Abdurazoqov <asrorbekabdurazzoqov99@gmail.com>
2025-03-29 18:00:23 +0500

    Create cosmetics.py

---
+++ cosmetics.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May  3 21:13:34 2024
+
+@author: Asilbek
+"""
+
+# I will start my projects since they are really interestedin learning them according to me
+#a=20
+##temp=36.6
+#print(type(a))
+#print(type(b))
+#ahali_soni=7_594_378_567
+#print("Aholi soni",ahali_soni)
+#x, y, z = 45, 2.5, -75
+#c=a*b
+#d=100//2
+#radius=20
+##diametr=2*radius
+#print("Aylna uzunlig",PI*diametr)
+#ism='Jobir'
+#yosh=36
+#xabar=ism + " " + str(yosh) + " yoshda"
+#print(xabar)
+#t_yil=int(input("Tug'ulgan yilingizni kiriting:"))
+#yosh=2024-(t_yil)
+
+#print("siz",yosh, "yoshda ekansiz")
+a=int("16")
+b=float("10")
+temp=str(36.6)
